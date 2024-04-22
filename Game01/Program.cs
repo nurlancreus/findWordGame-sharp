e43a4a47b81e75fa796d96d9f3be0265bd66f399
nurlancreus <nurlancreus007@gmail.com>
2024-04-22 17:15:13 +0400

    save changes

--- Game01/Program.cs
+++ Game01/Program.cs
@@ -2,55 +2,81 @@
 Console.WriteLine("Hello, World!");
 
 // start again 
-static void StartAgain()
+bool playGame = true;
+static void StartAgain(ref bool playGame)
 {
     Console.WriteLine("Try again?");
     var userInput = Console.ReadKey();
     Console.Clear();
+
     if (userInput.Key == ConsoleKey.Enter)
     {
         Console.WriteLine("You Start The Game Again!");
-        InitGame();
+        InitGame(playGame);
     }
     else
     {
         Console.Clear();
         Console.WriteLine("EXIT GAME");
+        playGame = false;
+        InitGame(playGame);
     }
 };
 
 // Find word Game
-static void InitGame()
+static void InitGame(bool playGame)
 {
+    if (!playGame) return;
+
     string hiddenWord = "Front-End";
     int attempts = 5;
-
     bool correctAnswer = false;
 
     Console.WriteLine("not \"Back-End\" but *****?");
-    do
+
+    while (!(correctAnswer || attempts == 0))
     {
-        string userAnswer = Console.ReadLine().ToLower();
-
-        if (userAnswer == hiddenWord.ToLower())
+        try
         {
-            correctAnswer = true;
-            Console.WriteLine("Correct Answer! YEHOOOOO");
-            StartAgain();
+            string userAnswer = Console.ReadLine().ToLower();
+
+            if (userAnswer == null)
+                throw new InvalidOperationException("Null input received."); // Throw exception if null input is received
+
+            if (userAnswer == hiddenWord.ToLower())
+            {
+                correctAnswer = true;
+                Console.WriteLine("Correct Answer! YEHOOOOO");
+                StartAgain(ref playGame);
+            }
+            else
+            {
+                Console.Clear();
+                Console.WriteLine($"Wrong Answer! Please try again. You got {--attempts}");
+            }
+
+            if (attempts == 0)
+            {
+                Console.Clear();
+                Console.WriteLine($"You LOST! The word was \"{hiddenWord}\"");
+                StartAgain(ref playGame);
+            }
+
         }
-        else
+        catch (InvalidOperationException e)
         {
             Console.Clear();
-            Console.WriteLine($"Wrong Answer! Please try again. You got {--attempts}");
+            Console.WriteLine("Invalid input. Please enter a string value.");
+            StartAgain(ref playGame);
         }
-
-        if (attempts == 0)
+        catch (Exception e)
         {
             Console.Clear();
-            Console.WriteLine($"You LOST! The word was {hiddenWord}");
-            StartAgain();
+            Console.WriteLine("An error occurred: " + e.Message);
+            StartAgain(ref playGame);
         }
-    } while (!correctAnswer || attempts == 0);
+
+    };
 }
 
-InitGame();
+if(playGame) InitGame(playGame);
